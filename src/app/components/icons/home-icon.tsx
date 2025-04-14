d8c8720d03fb7a2f876ee80948657dcc1e12856f
m0owo <8breadsticks8@gmail.com>
2025-04-14 16:32:31 +0700

    feat: add home, light, fan, ac icons

---
+++ src/app/components/icons/home-icon.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+const HomeIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    fill="none"
+    viewBox="0 -0.5 24 24"
+  >
+    <path
+      fill="#000"
+      fillRule="evenodd"
+      d="M13 21v-7a2 2 0 0 1 2-2h2a2 2 0 0 1 2 2v7h2.05v-8.925L12 7.14l-9 4.91V21h10zm10.05-8v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-7.999c-.523 0-1-.42-1-1.001V7a1 1 0 0 1 .521-.878l11-6a1 1 0 0 1 .958 0l11 6A1 1 0 0 1 24 7v5c0 .563-.447.974-.95 1zM2 10.316l9.521-5.194a1 1 0 0 1 .958 0L22 10.316V7.594L12 2.139 2 7.594v2.721zM15 14v7h2v-7h-2zm-8-2h2a2 2 0 0 1 2 2v2a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2v-2a2 2 0 0 1 2-2zm0 2v2h2v-2H7z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+export default HomeIcon;
